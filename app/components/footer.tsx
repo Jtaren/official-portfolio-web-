2ca5dfcd1788607874b99cb9494f8f41ab9c6a74
jtaren <arenjatau@gmail.com>
2025-01-11 22:52:29 +0100

    footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,22 +1,23 @@
+
 import React from 'react'
 import { Socials } from './socials'
-import { Copy ) from 'lucide-react'
+import { Copy } from 'lucide-react'
 import Image from 'next/image'
 import { LocationTime } from './locationTime'
 import EmailCopy from './emailCopy'
 import Link from 'next/link'
 
 export const Footer = () => {
-	return 
-		<footer className='space-y-[17px]'>
-			<div className=''>
-				<h2 className='title text-[23px]'>Let's Connect</h2>
-				<p className='font-light'>Any questions, proposals, or collaborations? Feel free to reach out.</p>
-			</div>
-			<div className='space-y-[17px]'>
-				<EmailCopy />
-				<Socials />
-			</div>
-		</footer>(
-	)
+  return (
+    <footer className='space-y-[17px]'>
+      <div className=''>
+        <h2 className='title text-[23px]'>Let's Connect</h2>
+        <p className='font-light'>Any questions, proposals, or collaborations? Feel free to reach out.</p>
+      </div>
+      <div className='space-y-[17px]'>
+        <EmailCopy />
+        <Socials />
+      </div>
+    </footer>
+  )
 }
